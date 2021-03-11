9c7fb5807e47fb47b2f434ee6c09f5854cf0b793
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-03-11 10:04:10 +0100

    Implemets bubble sort method

--- src/main/java/pid/exercise/sort/BubbleSort.java
+++ src/main/java/pid/exercise/sort/BubbleSort.java
@@ -3,7 +3,19 @@ package pid.exercise.sort;
 public class BubbleSort {
 
     public static void sort(int[] array) {
-        // TODO Implement bubble sort
+        boolean isSorted = false;
+        int temp;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i+1]) {
+                    temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                    isSorted = false;
+                }
+            }
+        }
     }
 
 }
